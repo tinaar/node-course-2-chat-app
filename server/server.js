e6e9233326e1b513fc725d842ee4fc674195b8b4
kwnstantina arnaoutaki <konstadina_ar@hotmail.com>
2018-10-19 18:06:42 +0300

    add geolocation suppport via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,35 +3,30 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-const {generateMessage}=require('./utils/message')
 app.use(express.static(publicPath));
 
-//io server instance 
 io.on('connection', (socket) => {
   console.log('New user connected');
-//from server to client -> server emits client listens this will be written on browser consloe client side
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-///from server to client side that will be logged on browser console
-  socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined') );
 
-  ///from client to server
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
   socket.on('createMessage', (message, callback) => {
-    ///this will be logged on console server received from client index.js 
     console.log('createMessage', message);
-    //server-> client 
-    io.emit('newMessage', generateMessage(message.from,message.text));
-    callback('This is from server');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
